daa41e8f7a05ed61dc1c3384c69aca4dffd841a2
riopablo10 <riopablofernando@gmail.com>
2021-07-04 20:17:33 -0300

    Segundo commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola riopablo10");
+	alert("Hola Division I");
 }
 
